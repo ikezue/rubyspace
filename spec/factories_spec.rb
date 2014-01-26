8e1ecdd70f021ea44a0d87802920baf0f57ca0c4
Chris Enedah <ikezue@gmail.com>
2014-01-26 13:50:41 -0800

    Use proper RSpec syntax in FactoryGirl spec

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -5,8 +5,8 @@ require 'factory_girl'
 
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "The #{factory_name} factory" do
-     it 'is valid' do
-      build(factory_name).should be_valid
-     end
+    it "is valid" do
+      expect(FactoryGirl.build factory_name).to be_valid
+    end
   end
 end
