e73bc8a5fd02a1f7739d1867d7f07ef10352a6f5
Chris Enedah <ikezue@gmail.com>
2014-01-26 21:20:20 -0800

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+problems =
+[
+  {
+    question: "Capitalize the first letter of each word.",
+    code: "words = ['paris', 'london', 'rome']",
+    solution: %{words.map(&:capitalize)},
+    answer: "['Paris', 'London', 'Rome']",
+    difficulty_level: 1
+  },
+
+  {
+    question: "Square each number in the numbers collection.",
+    code: "numbers = [1, 2, 3, 4, 5]",
+    solution: %{numbers.map {|n| n * n }},
+    answer: "[1, 4, 9, 16, 25]",
+    difficulty_level: 1
+  },
+
+  {
+    question: "Set the first letter of each word to lower case.",
+    code: "words = ['PARIS', 'LONDON', 'ROME']",
+    solution: %{words.map {|c| c[0].downcase + c[1..-1] }},
+    answer: "['pARIS', 'lONDON', 'rOME']",
+    difficulty_level: 3
+  }
+]
+
+problems.each do |problem|
+  Problem.find_or_create_by(question: problem[:question]) do |p|
+    p.code = problem[:code]
+    p.solution = problem[:solution]
+    p.answer = problem[:answer]
+    p.difficulty_level = problem[:difficulty_level]
+  end
+end
